e2731230e0803fd581f54a9e0586cdf213b94f80
wjdehddk123 <wjdehddk123@naver.com>
2023-10-05 22:24:17 +0900

    cors 정책

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,10 @@ import { HttpExceptionFilter } from "./common/exceptions/http_exception.filter";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: "https://port-0-now-meet-backend-k19y2klk157645.sel4.cloudtype.app",
+  });
+
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalFilters(new HttpExceptionFilter());
 
